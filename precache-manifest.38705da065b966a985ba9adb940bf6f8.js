ce1479565b29522793b85a4d9abb083ea7a04bde
Souvik50 <duttasouvik916@yahoo.in>
2019-08-03 14:26:37 +0530

    Updates

---
+++ precache-manifest.38705da065b966a985ba9adb940bf6f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23c5dcc06522b5c051f73ac1907a32b",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "00e99ca17ae1fce637d2",
+    "url": "/monsters/static/css/main.45170750.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "00e99ca17ae1fce637d2",
+    "url": "/monsters/static/js/main.a881d4b4.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
